8834312a58e6e3807e6963841ba268befb46f245
june0039 <74568248+june0039@users.noreply.github.com>
2022-02-10 14:16:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,12 @@
 -highlight current headings
 -float sidebar to the side*/
 
-const headings = document.querySelectorAll('h1, h2, h3');
+const $content = document.getElementById('content')
+const headings = document.querySelectorAll('h1, h2, h3')
 
+document.getElementById('sidebar')
+
+for(const $heading of headings) {
+  
+  const id  = $heading.textContent
+  $heading.setAttribute('id', 
