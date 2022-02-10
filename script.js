12757213115d60be162b89e88ca5f783fbc0e2ec
june0039 <74568248+june0039@users.noreply.github.com>
2022-02-10 14:23:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,21 @@
 const $content = document.getElementById('content')
 const headings = document.querySelectorAll('h1, h2, h3')
 
-document.getElementById('sidebar')
+const $sidebar = document.getElementById('sidebar')
 
 for(const $heading of headings) {
   
   const id  = $heading.textContent
-  $heading.setAttribute('id', 
+  $heading.setAttribute('id', id)
+  
+  $sidebar.innerHTML += '
+  <a href='#${id}' class='toc-link toc-${heading-tagName}'>${$heading.textContent}</a>'
+}
+
+let scrollPosition = 0
+
+document.addEventListener('scroll', function () {
+  
+  scrollPosition = window.scrollY
+  
+})
